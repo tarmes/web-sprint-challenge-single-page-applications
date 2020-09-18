8ede5b2e37e0e03b2b8303544f445a614d918e00
tarmes <traarmes@gmail.com>
2020-09-18 11:48:55 -0400

    reactstrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { NavLink } from 'reactstrap'
 
 export default function Home() {
     return (
-        <h1>THIS IS THE HOME PAGE</h1>
+        <div className='container'>
+            <div className='nav-container'>
+                <nav>
+                    <NavLink tag={Link} to='/orderYourPizza'>Order now!</NavLink>
+                </nav>
+            </div>
+            <div className ='home-section'>
+                <h1>THIS IS THE HOME PAGE</h1>
+            </div>
+        </div>
+        
     )
 }
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { NavLink } from 'reactstrap'
 
 export default function PizzaForm() {
-    return (
-        <h2>THIS IS THE PIZZA FORM</h2>
+    return (        
+        <div className='container'>
+            <div className='nav-container'>
+                <nav>
+                    <NavLink tag={Link} to='/'>Go home!</NavLink>
+                </nav>
+            </div>
+            <div className ='pizzaForm-section'>
+                <h2>THIS IS THE PIZZA FORM</h2>
+            </div>
+    </div>
     )
 }
