1296d15818a0baed92562c0afd8829cdd4c6814e
tarmes <traarmes@gmail.com>
2020-09-18 11:21:38 -0400

    components built

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Home from './components/Home'
+import PizzaForm from './components/PizzaForm'
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Home />
+      <PizzaForm />
     </>
   );
 };
---
+++ src/components/PizzaForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PizzaForm() {
+    return (
+        <h2>THIS IS THE PIZZA FORM</h2>
+    )
+}
